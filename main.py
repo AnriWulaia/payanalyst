3cd279f63a2874e923ba50cf9194d4bacb18d2f7
AnriWulaia <wulaia.anri@gmail.com>
2026-06-14 13:11:48 +0400

    rate limiter for ai api

--- main.py
+++ main.py
@@ -10,9 +10,18 @@ from report import generate_pdf
 from dotenv import load_dotenv
 from fastapi.middleware.cors import CORSMiddleware
 
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
+
 load_dotenv()
 
+
+limiter = Limiter(key_func=get_remote_address)
 app = FastAPI()
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -47,6 +56,8 @@ async def stats_only(file: UploadFile = File(...)):
     return analyze(df)
 
 @app.post("/analyze")
+@limiter.limit("10/hour")
+
 async def analyze_file(file: UploadFile = File(...)):
     if not file.filename.endswith(".csv"):
         raise HTTPException(status_code=400, detail="Only CSV files are supported")
